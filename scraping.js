268320ea53630aab3ec528dc24382e02c43cff0c
Kauã Richard <Kau.lira@yahoo.com>
2025-09-17 20:04:20 -0300

    feito

---
+++ scraping.js
@@ -0,0 +1,9 @@
+let infosboss = document.querySelectorAll(".col-sm-4");
+
+for (let i = 0; i < infoboss.length; i++) { 
+    if (infoboss[i].innerText.includes("Legend")) {
+        console.log(infosboss[i].innerText);
+    }
+}
+
+//https://eldenring.wiki.fextralife.com/Bosses
