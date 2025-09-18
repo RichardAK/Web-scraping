d4409e9b4a580a445f63f9b0b032da9851f742f1
RichardAK <100002016+RichardAK@users.noreply.github.com>
2025-09-18 19:51:20 -0300

    Update scraping.js

--- scraping.js
+++ scraping.js
@@ -1,6 +1,6 @@
-let infosboss = document.querySelectorAll(".col-sm-4");
+let infoboss = document.querySelectorAll(".col-sm-4");
 
-for (let i = 0; i < infosboss.length; i++) { 
+for (let i = 0; i < infoboss.length; i++) { 
     if (infoboss[i].innerText.includes("Legend")) {
         console.log(infosboss[i].innerText);
     }
